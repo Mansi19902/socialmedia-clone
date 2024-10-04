b46e18ad3a355ecb1af675216aa0c9c66d10a598
Mansi <mansidiwakar467@gmail.com>
2024-10-04 21:23:35 +0530

    first commit

---
+++ frontend/src/pages/AuthPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginCard from "../components/LoginCard";
+import { useRecoilValue } from "recoil";
+import authScreenAtom from "../atoms/authAtom";
+import SignupCard from "../components/SignupCard";
+
+const AuthPage = () => {
+  const authScreenState = useRecoilValue(authScreenAtom);
+  return <>{authScreenState === "login" ? <LoginCard /> : <SignupCard />}</>;
+};
+
+export default AuthPage;
